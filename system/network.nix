6429360004b4a5e1a44e26e795ef4c3b9868ebf9
non-existent <nobody@nonexist.com>
2022-12-29 21:12:24 +0800

    add wireguard

--- system/network.nix
+++ system/network.nix
@@ -24,4 +24,26 @@
     enable = true;
     configFile = toString ./v2ray.json;
   };
+  networking.wireguard.interfaces = {
+    wg0= {
+      ips = [ "10.100.0.1/24" ];
+      listenPort = 51820;
+      postSetup = ''
+                ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+                '';
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+      privateKeyFile = "/etc/nixos/system/private";
+      peers = [
+       {
+         publicKey = "k1aEyFLOBJt/2BvPkoYfjTfZnDzKb8IpbgpLEZUPgzA=";
+         allowedIPs = ["10.100.0.2/32"];
+         persistentKeepalive = 60;
+         endpoint = "[2409:8a70:a4b:1bf0:12e7:ee97:5f90:52ee]:51820";
+       }
+
+     ];
+    };
+  };
 }
