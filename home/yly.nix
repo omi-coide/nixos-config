7f8171901a3df9ec220d141ab596362ebe1970de
non-existent <nobody@nonexist.com>
2023-02-09 23:39:16 +0800

    reformat only

--- home/yly.nix
+++ home/yly.nix
@@ -1,7 +1,7 @@
 { inputs, system, config, pkgs, ... }:
 let
   mma = pkgs.callPackage ../pkgs/homemade/mathematica/default.nix { version = "13.1.0"; lang = "cn"; };
-  nurpkgs = (with inputs.ylynur.packages.${system}; [test-app ]);
+  nurpkgs = (with inputs.ylynur.packages.${system}; [ test-app ]);
   utils = (with pkgs; [
     # unix tools
     htop
