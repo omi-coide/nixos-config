a531151f912b09b1e55caebcb3c03a8512f6058c
non-existent <nobody@nonexist.com>
2023-03-16 21:19:57 +0800

    add bind whois,disable proxy

--- system/network.nix
+++ system/network.nix
@@ -11,9 +11,9 @@
   networking.iproute2.enable = true;
 
   # Configure network proxy
-  networking.proxy.default = "http://127.0.0.1:12333";
-  networking.proxy.allProxy = "http://127.0.0.1:12333";
-  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # networking.proxy.default = "http://127.0.0.1:12333";
+  # networking.proxy.allProxy = "http://127.0.0.1:12333";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Open ports in the firewall.
   #networking.firewall.enable = true;
