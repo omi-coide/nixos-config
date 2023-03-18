02e8233f5a175ba76d7297d677396ba384ec37ea
non-existent <nobody@nonexist.com>
2023-03-18 22:09:56 +0800

    can't git push with ssh without proxy

--- system/network.nix
+++ system/network.nix
@@ -11,12 +11,12 @@
   networking.iproute2.enable = true;
 
   # Configure network proxy
-  # networking.proxy.default = "http://127.0.0.1:12333";
-  # networking.proxy.allProxy = "http://127.0.0.1:12333";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.proxy.default = "http://127.0.0.1:12333";
+  networking.proxy.allProxy = "http://127.0.0.1:12333";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Open ports in the firewall.
-  #networking.firewall.enable = true;
+  networking.firewall.enable = false;
   #networking.firewall.allowedTCPPorts = [ 22 80 ];
   #networking.firewall.allowedUDPPorts = [ ];
 
