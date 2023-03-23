cba50057dab286f848ec4eae3528bfc2d2581986
non-existent <nobody@nonexist.com>
2023-03-23 20:39:14 +0800

    upgrade kernel to 6.2

--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -14,7 +14,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   fileSystems."/" =
     {
       device = "/dev/disk/by-uuid/7908a415-b26b-40d0-87c8-984e154389df";
